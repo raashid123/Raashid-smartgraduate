302fe90bd6ad2ebf127dbb02d2318f3a24195f94
AbdulRaashid <113176076+AbdulRaashid@users.noreply.github.com>
2022-09-29 17:11:19 +0530

    Delete 9.c

--- 9.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,c;
-    printf("enter two numbers to add \n");
-    scanf("%d%d",&a,&b);
-    c=a+b;
-    printf("sum of entered numbers is = %d\n",c);
-}
